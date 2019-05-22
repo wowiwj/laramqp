fa9117d4afb375105261aba8b495a85a7038ab60
wangju <wangju@shein.com>
2019-05-22 15:47:05 +0800

    first commit

---
+++ tests/Feature/BaseUsageTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Laramqp\Tests\Feature;
+
+use Laramqp\Tests\TestCase;
+
+class BaseUsageTest extends TestCase
+{
+    public function test_base_usage()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Laramqp\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    public function setUp()
+    {
+    }
+}
